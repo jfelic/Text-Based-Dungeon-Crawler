674f7bde33ae59aa2bd71e723a682fb333813ebe
Julian Feliciano <felicianojulian1@gmail.com>
2023-11-21 13:21:54 -0500

    Finished all interactions with the Mysterious Merchant.
    Next I will begin to work on combat

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
         System.out.println("## Welcome to Candy Cavern! ##");
         System.out.println("--------------------------------------");
-        System.out.println("Main Menu:");
         System.out.println("1. Start");
         System.out.println("2. Credits");
         System.out.println("3. Exit");
@@ -20,8 +19,22 @@ public class Main {
         // Handling user input from the menu //
         if (userInput == 1) {
             System.out.println("--------------------------------------");
-            System.out.println("Greetings adventurer! I have a very important request, and from the looks of you, you're the one for the job.");
-            Player player = new Player(in);
+            System.out.println("Mysterous Merchant: \"Greetings adventurer! I have a very important request, and from the looks of you, you're the one for the job.");
+            System.out.println("The Candy King of Candy Cavern and his minions have been causing mayhem near our village and none of the traderoutes are safe.");
+            System.out.println("Something must be done! What say you?\"");
+            System.out.println("\n1. \"Don't worry, I will take care of the Candy King and his minions for you!\"\n2. \"No thanks, I'm busy.\"");
+            int responseToMerchant = in.nextInt();
+            in.nextLine();
+            if (responseToMerchant == 1) {
+                System.out.println("Mysterious Merchant: \"I knew I was right about you! Now then, what should I call you adventurer?\"");
+                System.out.print("Input name: ");
+                Player player = new Player(in);
+            } else if (responseToMerchant == 2) {
+                System.out.println("\nMysterious Merchant: \"No? It seems I was wrong about you... good day then!\"");
+                System.out.println("Your journey ends before it even began. Not a very good run, but thanks for playing anyways!");
+                System.out.println("--------------------------------------");
+                System.out.println();
+            }
         } else if(userInput == 2) {
             System.out.println("Created by Julian Feliciano");
             System.out.println("2023");
@@ -29,7 +42,37 @@ public class Main {
             System.out.println("You've successfully exited, thank you for playing!");
         }
 
-        // After Character Creation //
+        // Saying Goodbye to the merchant //
+        System.out.println("Mysterious Merchant: \"Well then, it looks like you're ready. Farewell and good luck!\"");
+        System.out.print("Say your goodbye to the merchant: ");
+        String goodbye = in.nextLine();
+        System.out.println();
+        System.out.println("With your goodbye said and done, you look down the path laid out before you. Looming far and high above is Candy Cavern. You begin walking....");
+        System.out.println("--------------------------------------");
 
-    }
+        // Entering the Cavern //
+
+        // //Testing combat//
+        // System.out.println("You encounter a " + Enemy enemy = new Enemy(rand));
+
+
+
+
+
+
+
+
+        // // Main Methods //
+        // void displayCombatOptions() {
+        //     System.out.println("Choose an action:");
+        //     System.out.println("1. Attack");
+        //     System.out.println("2. Use Potion");
+        //     System.out.println("3. Flee");
+        // }
+
+        // public void performAttack (Player player, Enemy enemy) {
+        //     int damage = calculateDamage(player, enemy);
+        //     enemy.takeDamage(damage);
+        //     System.out.println("You dealt " + damage + " damage to the " + enemy.getType());
+        }
 }
--- src/Player.java
+++ src/Player.java
@@ -15,35 +15,46 @@ public class Player {
     //int healthPotionDropChance = 50; //Percentage
 
     Player (Scanner in) {
-        System.out.println("What is your name?: ");
         this.name = in.nextLine();
-        System.out.println("\nOkay " + name + ", " + "how would you describe yourself?");
+        System.out.println("--------------------------------------");
+        System.out.println("Mysterious Merchant: \"Okay " + name + ", " + "how would you describe yourself?\"");
         System.out.println("1. Strong and reliable\n2. Intelligent/thinker\n3. Accurate and to the point\n4. Cunning when need be");
         fighterType = in.nextInt();
         in.nextLine();
 
         // Determining passive ability //
-        if(this.fighterType == 1) {
+        do {
+            if(this.fighterType == 1) {
             this.passive = "Candy Crusher";
             this.maxAttackDamage = 60;
             System.out.println("--------------------------------------");
-            System.out.println("Oh so you're a Candy Crusher then, I see.");
-        } else if(this.fighterType == 2){
+            System.out.println("Mysterious Merchant: \"Oh so you're a Candy Crusher then, I see.\"");
+            break;
+            } else if(this.fighterType == 2){
             this.passive = "Wizard";
             this.numOfPotions = 5;
             System.out.println("--------------------------------------");
-            System.out.println("Oh so you're a Wizard then, I see.");
-        } else if(this.fighterType == 3) {
+            System.out.println("Mysterious Merchant: \"Oh so you're a Wizard then, I see.\"");
+            break;
+            } else if(this.fighterType == 3) {
             this.passive = "Archer";
             this.accuracy = 95;
             System.out.println("--------------------------------------");
-            System.out.println("Oh so you're a Archer then, I see.");
-        } else {
+            System.out.println("Mysterious Merchant: \"Oh so you're a Archer then, I see.\"");
+            break;
+            } else if(this.fighterType == 4){
             this.passive = "Rogue";
             this.dodgePercentage = 20;
             System.out.println("--------------------------------------");
-            System.out.println("Oh so you're a Rogue then,");
+            System.out.println("Mysterious Merchant: \"Oh so you're a Rogue then,\"");
+            break;
+            } else {
+            System.out.println("--------------------------------------");
+            System.out.println("## Invalid Input ##");
+            fighterType = in.nextInt();
+            in.nextLine();
         }
+    } while (1 != 0);
 
         /* ---Stat Allocation---
          * maxHealth
@@ -54,7 +65,7 @@ public class Player {
          */
         for(int i = 5; i > 0; i--) {
             System.out.println("You have " + i + " stat points remaining to allocate. Where would you like to put them?");
-            System.out.println("\n1. Max Health\n2. Max Damage\n3. Accuracy\n4. Dodge Chance\n5. Luck");
+            System.out.println("\n1. Max Health (max health + 5)\n2. Max Damage (max damage + 2)\n3. Accuracy (accuracy + 2)\n4. Dodge Chance (dodge chance + 2)\n5. Luck (luck + 5)");
             int userStatChoice = in.nextInt();
             if (userStatChoice == 1) {
                 this.maxHealth += 5;
@@ -65,17 +76,21 @@ public class Player {
                 System.out.printf("Max Health: %d\nMax Damage: %d\nAccuracy: %d\nDodge Percentage: %d\nLuck: %d\n", this.maxHealth, this.maxAttackDamage,this.accuracy,this.dodgePercentage,this.luck);
                 System.out.println("--------------------------------------");
             } else if (userStatChoice == 3) {
-                this.accuracy += 1;
+                this.accuracy += 2;
                 System.out.printf("Max Health: %d\nMax Damage: %d\nAccuracy: %d\nDodge Percentage: %d\nLuck: %d\n", this.maxHealth, this.maxAttackDamage,this.accuracy,this.dodgePercentage,this.luck);
                 System.out.println("--------------------------------------");
             } else if (userStatChoice == 4) {
                 this.dodgePercentage += 2;
                 System.out.printf("Max Health: %d\nMax Damage: %d\nAccuracy: %d\nDodge Percentage: %d\nLuck: %d\n", this.maxHealth, this.maxAttackDamage,this.accuracy,this.dodgePercentage,this.luck);
                 System.out.println("--------------------------------------");
-            } else {
+            } else if (userStatChoice == 5){
                 this.luck += 5;
                 System.out.printf("Max Health: %d\nMax Damage: %d\nAccuracy: %d\nDodge Percentage: %d\nLuck: %d\n", this.maxHealth, this.maxAttackDamage,this.accuracy,this.dodgePercentage,this.luck);
                 System.out.println("--------------------------------------");
+            } else {
+                System.out.println("--------------------------------------");
+                System.out.println("## Invalid input ##");
+                i++;
             }
         }
         in.nextLine();
